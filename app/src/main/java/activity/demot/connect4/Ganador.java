ec562fd7637aef1b96ffdecb8be8bd4fd762f76f
batistafull <albertomimi16@gmail.com>
2017-12-16 12:30:00 -0300

    Implementación del sharePreference

--- app/src/main/java/activity/demot/connect4/Ganador.java
+++ app/src/main/java/activity/demot/connect4/Ganador.java
@@ -1,13 +1,20 @@
 package activity.demot.connect4;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class Ganador extends AppCompatActivity {
-
+    SharedPreferences prefs;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ganador);
+        SharedPreferences prefs = getSharedPreferences("Historial", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = prefs.edit();
+        editor.putString("puntuacion", "100");
+        editor.commit();
+
     }
 }
--- app/src/main/java/activity/demot/connect4/Puntuacion.java
+++ app/src/main/java/activity/demot/connect4/Puntuacion.java
@@ -1,13 +1,20 @@
 package activity.demot.connect4;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class Puntuacion extends AppCompatActivity {
-
+    SharedPreferences prefs;
+    private TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_puntuacion);
+        textView = (TextView) findViewById(R.id.text1);
+        SharedPreferences prefs = getSharedPreferences("Historial", Context.MODE_PRIVATE);
+        textView.setText(prefs.getString("puntuacion", ""));
     }
 }
