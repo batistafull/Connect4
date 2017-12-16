c4e779b5f4ef053a07348218da42a20856233c2e
batistafull <albertomimi16@gmail.com>
2017-12-16 11:50:13 -0300

    pasar activity a puntuacion.class y acerca.class

--- app/src/main/java/activity/demot/connect4/MainActivity.java
+++ app/src/main/java/activity/demot/connect4/MainActivity.java
@@ -7,16 +7,23 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     private ImageButton btnPlay;
     private ImageView imageView;
+    private TextView txt1;
+    private ImageButton imgBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         imageView = (ImageView) findViewById(R.id.imgportada);
         btnPlay = (ImageButton) findViewById(R.id.play);
+        imgBtn = (ImageButton) findViewById(R.id.puntuacion);
+        txt1 = (TextView) findViewById(R.id.acerca);
+        txt1.setOnClickListener(this);
+        imgBtn.setOnClickListener(this);
         btnPlay.setOnClickListener(this);
     }
 
@@ -27,6 +34,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         switch (btnId){
             case R.id.play:
                 intent = new Intent(this, Jugar.class);
+            break;
+            case R.id.puntuacion:
+                intent = new Intent(this, Puntuacion.class);
+            break;
+            case R.id.acerca:
+                intent = new Intent(this, Acerca.class);
             break;
         }
         startActivity(intent);
